a95094206d9323d2cc093cd2bcb8f193de3f0ed9
Jovanni Ramirez <jovanni_r15@yahoo.com>
2024-10-10 01:34:48 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val buttonLeft = findViewById<Button>(R.id.buttonLeft)
+        val buttonRight = findViewById<Button>(R.id.buttonRight)
+        val scoreView = findViewById<TextView>(R.id.ScoreView)
+
+        var leftNum = Random.nextInt(1, 10)
+        var rightNum = Random.nextInt(1, 10)
+        while (leftNum == rightNum) {
+            rightNum = Random.nextInt(1, 10)
+        }
+        buttonLeft.setText(leftNum.toString())
+        buttonRight.setText(rightNum.toString())
+
+        var score = 0
+
+        //set onClick listeners
+        buttonLeft.setOnClickListener{
+            leftNum = buttonLeft.getText().toString().toInt()
+            rightNum = buttonRight.getText().toString().toInt()
+            if (leftNum > rightNum) {
+                score++
+                Toast.makeText(this@MainActivity, "Win!", Toast.LENGTH_LONG).show()
+            }
+            else {
+                score--
+            }
+            scoreView.setText("Score: $score")
+
+            leftNum = Random.nextInt(1, 10)
+            rightNum = Random.nextInt(1, 10)
+            while (leftNum == rightNum) {
+                rightNum = Random.nextInt(1, 10)
+            }
+            buttonLeft.setText(leftNum.toString())
+            buttonRight.setText(rightNum.toString())
+
+        }
+
+        buttonRight.setOnClickListener{
+            leftNum = buttonLeft.getText().toString().toInt()
+            rightNum = buttonRight.getText().toString().toInt()
+            if (leftNum < rightNum) {
+                score++
+                Toast.makeText(this@MainActivity, "Win!", Toast.LENGTH_LONG).show()
+            }
+            else {
+                score--
+            }
+            scoreView.setText("Score: $score")
+
+            leftNum = Random.nextInt(1, 10)
+            rightNum = Random.nextInt(1, 10)
+            while (leftNum == rightNum) {
+                rightNum = Random.nextInt(1, 10)
+            }
+            buttonLeft.setText(leftNum.toString())
+            buttonRight.setText(rightNum.toString())
+
+        }
+    }
+}
